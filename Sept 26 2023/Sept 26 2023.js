c32e626ce05eb9a035244e270ef6034a11cf0ccd
Leanne Keenan <l_keenan@outlook.com>
2023-09-26 12:56:39 -0700

    Added daily Review Session
    Reviewed concepts of SMACSS, mobile-first, and the performance pitfalls of Web Dev

---
+++ Sept 26 2023/Sept 26 2023.js
@@ -0,0 +1,44 @@
+console.log('1. What is the 80/20 rule?')
+console.log('A principle that implies 80% of a projects outcome can be attributed to 20% of the effort')
+console.log('')
+
+console.log('2. Scaleable Modular Architecture: Theme')
+console.log('A principle of content creation and organozation stating like content should be grouped to create thematic sections of a page')
+console.log('')
+
+console.log('3. Scaleable Modular Architecture: State')
+console.log('Identifies the different states each page elementis in when default or active')
+console.log('')
+
+console.log('4. Two largest/ most common pitfalls to preformance in web dev')
+console.log('Using high res/uncompressed images or placing JS in the head of a doc without using async')
+console.log('')
+
+console.log('5. Flexible Grid');
+console.log('A formating style that allows the content to stay in the flow of a page on any viewport using media queries')
+console.log('')
+
+console.log('6. How do you centralize your design-related settings within your CSS file?')
+console.log([
+    '1. use a CSS preprocessor (like Sass or Less)',
+    '2. Organize by sections (like typography, colors, sizes, etc.)',
+    '3. Use comments',
+    '4. Create variable sections',
+    '5. Avoid Global Styles',
+    '6. Use a seperate file for Design Tokens'
+])
+console.log('')
+
+console.log('7. Mobile First Approach')
+console.log('A concept of building apps and websites for mobile devices then expanding larger to bigger viewport devices')
+console.log('')
+
+console.log('8. Scaleable Modular Architecture: Base')
+console.log('The default styles of HTML elements (typography, height, font-size)')
+console.log('')
+
+console.log('9. Scaleable Modular Architecture: Layout')
+console.log('overall structure and layout of a web page or sections of a page.')
+console.log('')
+console.log('10. Scaleable Modular Architecture: Module')
+console.log('The individual elements of a webpage')
