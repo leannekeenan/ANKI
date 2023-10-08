1e00f76ff72deee9b9330f1260011f078d025316
Leanne Keenan <l_keenan@outlook.com>
2023-10-08 12:33:00 -0700

    Added Daily Review Session

--- Oct 08 2023/Oct 08 2023.js
+++ Oct 08 2023/Oct 08 2023.js
@@ -53,26 +53,46 @@ console.log('')
 
 /*1Codewars Challenge*/
 // .
-console.log('')
-console.log('')
+console.log('filter the number')
+function filterTheNumber(str) {
+    console.log(str.split('').filter((letters) => !/[0-9]/.test(letters)).join(''))
+}
+filterTheNumber('this has 3 numbers')
 console.log('')
 
 //2 .
-console.log('')
-console.log('')
+console.log('You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party')
+function undergroundLeague(pokemon) {
+    for(let i = 1; i <= pokemon; i++) {
+        console.log(`I choose you ${i}` )
+    }
+}
+undergroundLeague(3)
 console.log('')
 
 //3 .
-console.log('')
-console.log('')
+console.log('Write function RemoveExclamationMarks which removes all exclamation marks from a given string.')
+function removeExclaimationMaarks(str) {
+    console.log(str.replaceAll("!", ""));
+}
+
+removeExclaimationMaarks('Hello! My name is Leanne.  It\'s so nice to meet you!!!')
 console.log('')
 
 //4 .
-console.log('')
-console.log('')
+console.log('Remove first and last character')
+function removeFirstAndLastCharacter(str) {
+    console.log(str.slice(1, -1))
+}
+
+removeFirstAndLastCharacter('Remove first and last character')
 console.log('')
 
 //5 .
-console.log('')
-console.log('')
+console.log('Disemvowel Trolls')
+function disemvowel(str) {
+    let vowels = /[aeiou]/ig;
+    console.log(str.replace(vowels, ''))
+}
+disemvowel('disemvowel')
 console.log('')
