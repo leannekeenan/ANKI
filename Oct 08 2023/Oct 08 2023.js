4869af1fa5082c96f02fe37d0d6ef2f4e377af66
Leanne Keenan <l_keenan@outlook.com>
2023-10-08 09:50:00 -0700

    Added Daily Review Session

---
+++ Oct 08 2023/Oct 08 2023.js
@@ -0,0 +1,78 @@
+/*ANKi Review: CSS*/
+//1 .
+console.log('Audio source Options')
+console.log('.ogg, .wav, .mp3')
+console.log('')
+
+//2 .
+console.log('How can you control if or when an element repeats?')
+console.log('background-reprat property with no-repeat value')
+console.log('')
+
+//3 .
+console.log('General Sibling Selector')
+console.log('use the tilde (~) operator between selectors to select a sibling element')
+console.log('')
+
+//4 .
+console.log('Target Width Formula')
+console.log('container width * percentage')
+console.log('')
+
+//5 .
+console.log('Modular CSS')
+console.log('a way of ordanizing CSS based on selector purpose within the website  including base, layout, stste, theme, and, modules.')
+console.log('')
+
+//6 .
+console.log('Adjcent Selectors')
+console.log('The addition operator (+) can be used to selects elements next to each other')
+console.log('')
+
+//7 .
+console.log('<input type=“submit”>')
+console.log('Used in forms to send form information through a server to a nother location like an email')
+console.log('')
+
+//8 .
+console.log('<option> elelment')
+console.log('used in select dropdown menus as independent options within the list')
+console.log('')
+
+//9 .
+console.log('What are Pixels')
+console.log('A measurement of lwngth in CSS that measures to 1/96th of an inch')
+console.log('')
+
+//10 .
+console.log('<caption>')
+console.log('An HTML tag used in tables to add descriptions of the table')
+console.log('')
+
+
+
+/*1Codewars Challenge*/
+// .
+console.log('')
+console.log('')
+console.log('')
+
+//2 .
+console.log('')
+console.log('')
+console.log('')
+
+//3 .
+console.log('')
+console.log('')
+console.log('')
+
+//4 .
+console.log('')
+console.log('')
+console.log('')
+
+//5 .
+console.log('')
+console.log('')
+console.log('')
