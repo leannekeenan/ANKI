02fda24ff21d5dac16cfdad8f64e8691fb1981ee
Leanne Keenan <l_keenan@outlook.com>
2023-11-18 12:04:20 -0800

    Added Daily Training Session

---
+++ 2023/Nov 18 2023/Nov 18 2023.js
@@ -0,0 +1,80 @@
+/*ANKI Review: */
+console.log('ANKI Review: General Knowledge')
+//1.
+console.log('1. MDN')
+console.log('Mozilla Developers Network.  A centralizeed data hub and learning resource for developers to find information on web technologies')
+console.log('')
+
+//2.
+console.log('2. SMACSS: State')
+console.log('A semantic section of CSS used to encapsulate the multiple pseudo class state types of a DOM element including states like hidden and clicked')
+console.log('')
+
+//3.
+console.log('3. Flexible Media')
+console.log('On objects like iframes and img maintaining aspect ration regardless of device screen size')
+console.log('')
+
+//4.
+console.log('4. Programming Language')
+console.log('A set of instructions translated into a language a computer can understand used to execute a function')
+console.log('')
+
+//5.
+console.log('5. How do you make embedded media flexible')
+console.log('Set the embedded media object into a block-level element like a div and used the containing element (the parent) to manipulate the embedded media (the child')
+console.log('')
+
+//6.
+console.log('6. how to measure compresson')
+console.log('measured by comparing the size of the original file with the size of the compressed file. The compression ratio, which is the ratio of the original file size to the compressed file size, is a common metric used to measure file compression.  You can manually compresss the file, use an online tool like compressor.io, use a task runner, or a CDN')
+console.log('')
+
+//7.
+console.log('7. How do you centralize your design related settings within your CSS file')
+console.log('Use SMACSS to semantically seperate teh code, keep the CSS in a seperate file from the program\'s HTML and JS, use comments, breakdown stylesheet into smaller, object based stylesheets')
+console.log('')
+
+//8.
+console.log('8. Scaleable Modular Architecture')
+console.log('A process of semantically formatting the properties of a CSS stylesheet based in their intent and use.  Allows for the code to be more readable and easier to manage.  The sections of Scaleable Modular Architecture include layout, module, theme , base and state')
+console.log('')
+
+//9.
+console.log('9. benedit of short selectors')
+console.log('Makes code easier to read, minimizes specificity, improves inheritance and efficiency')
+console.log('')
+
+//10.
+console.log('10. golden rule of front end development')
+console.log('Rule of seperation of concerns.  Keep content with HTML, design with CSS and function with JS')
+console.log('')
+
+
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+//1.
+console.log('1. ')
+console.log('')
+console.log('')
+
+//2.
+console.log('2. ')
+console.log('')
+console.log('')
+
+//3.
+console.log('3. ')
+console.log('')
+console.log('')
+
+//4.
+console.log('4. ')
+console.log('')
+console.log('')
+
+//5.
+console.log('5. ')
+console.log('')
+console.log('')
