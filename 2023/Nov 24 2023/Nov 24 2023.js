e368558c323f999f8603cd1d43684f9a406603df
Leanne Keenan <l_keenan@outlook.com>
2023-11-24 14:15:40 -0800

    Added Daily Training Session

--- 2023/Nov 24 2023/Nov 24 2023.js
+++ 2023/Nov 24 2023/Nov 24 2023.js
@@ -127,26 +127,92 @@ console.log('')
 /*Codewars Challenges*/
 console.log('Codewars Challenges')
 //1.
-console.log('1. ')
-console.log('')
-console.log('')
+console.log('1. ROT13')
+console.log('Create a cypher that takes in a string and returns a new string with the letters of the string shifted forward 13 letters')
+let rot13 = (string) => {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklm';
+    return string.replace(/[a-z]/gi, c => alphabet[alphabet.indexOf(c) + 13])
+    
+}
+console.log(rot13('hello'))
+ console.log('')
 
 //2.
-console.log('2. ')
-console.log('')
+console.log('2. feast of many beast')
+console.log('write a function that takes a name of an animal and a name of a dish they want to being to a potluck. The dish they bring mush meet the  condition of having the first letter and last letter of the animals name match the first lettter and last letter of the dish they are trying to bring.  Return a boolean response ')
+
+let feast = (animal, dish) => {
+    if(animal[0] === dish[0] 
+        && 
+        animal[animal.length - 1] === dish[dish.length - 1]) {
+        console.log(true)
+    }
+    else {
+        console.log(false)
+    }   
+}
+
+feast('cat', 'carrot')
 console.log('')
 
 //3.
-console.log('3. ')
-console.log('')
+console.log('3. ATM')
+console.log('write a function that determines the minimal number of banknotes needed make a cash withdrawl of no less than 1 dollar and no more than 1500.  Return -1 if the withdrawl cant be made')
+let ATM = (withdrawl) => {
+    let banknotes = [1, 5, 10, 20, 50, 100];
+    let result = {};
+    console.log(withdrawl)
+    for(let i = banknotes.length - 1; i >= 0; i--) {
+        
+        while(withdrawl >= banknotes[i]) {
+            
+            if(result[banknotes[i]]) {
+                result[banknotes[i]]++
+            }
+            else {
+                result[banknotes[i]] = 1
+            }
+
+            withdrawl -= banknotes[i]
+        }
+    }
+    console.log(result)
+}
+ATM(101)
 console.log('')
 
 //4.
-console.log('4. ')
-console.log('')
+console.log('4. clock')
+console.log('write a function that returns how much time has passed since midnight in miliseconds')
+let clock = (h, m , s) => {
+    let second = s * 1000;
+    let minute = m * 60000;
+    let hour = h * 3600000;
+
+    console.log(hour + minute + second)
+}
+
+clock(1, 1, 1);
 console.log('')
 
 //5.
-console.log('5. ')
-console.log('')
+console.log('5. sum of multiples')
+console.log('sum all multiples of n below m.  Return invalid if less than 0')
+let sumOfMultiples = (n, m) => {  //n = divisor, m = bound
+let total = 0
+let numbers = []
+for(let i = 1; i * n <= m; i++) {
+    total += i * n
+    numbers.push(i)
+}
+if(total > 0) {
+    console.log(numbers)
+    console.log(total)
+}
+else {
+    console.log(numbers)
+    console.log('invalid')
+}
+}
+sumOfMultiples(1, 5)
 console.log('')
