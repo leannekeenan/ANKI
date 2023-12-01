0e971739be25d70c437ac9839d2f6bee08c8ed94
Leanne Keenan <l_keenan@outlook.com>
2023-12-01 14:20:02 -0800

    Added Daily Training Session

---
+++ 2023/Dec 01 2023/script.js
@@ -0,0 +1,81 @@
+/*ANKI Review: CSS*/
+console.log('ANKI Review: CSS')
+//1.
+console.log('1. Display Property')
+console.log('Used to set the display mode for an object.  This can include block, flex, flexbox, inline and inline-block.  These represent the display state of the object')
+console.log('')
+
+//2.
+console.log('2. inline element')
+console.log('element that stays within flow of the DOM and only takes up as much space as needed to fit the content.  Additional space can be shared by other inline elements or if embedded withoin another element.  Examples include the span, strong, em, img tags and more')
+console.log('')
+
+//3.
+console.log('3. When to use blockquote')
+console.log('when qouting a external source')
+console.log('')
+
+//4.
+console.log('4. property value relative')
+console.log('used to position elements relative to their position within their parent containers.  If not within a specified parent container, the element is positioned relative to the DOM body.  This allows the element to use offset values for top, left, right, and bottom to manipulate the position of the element within its parent on the DOM')
+console.log('')
+
+//5.
+console.log('5. box-shadow property')
+console.log('accepts four values to provide a shadow effect to the content container.  These values represenr vertical offeset of shadow, horizontal offset of shadow, blur, and color')
+console.log('')
+
+//6.
+console.log('6. vertival align')
+console.log('used on inline, inline-block, and table cell elements to align items top to bottom on the page. can accept values like top middle and bottom as well as numerical values')
+console.log('')
+
+//7.
+console.log('7. position absolute')
+console.log('a value for the position property setting an element position outside of the flow of the document positioning the container in absolute relativity to the nearest parent container')
+console.log('')
+
+//8.
+console.log('8. web-fafe font')
+console.log('a back up styling for specialized font types used as a fallback on devices that can not render specialized characters.  These include: "Times New Roman", "Arial", "Courier New" and "Georgia".')
+console.log('')
+
+//9.
+console.log('9. whas the border-box property for?')
+console.log('Border box provides elelments with a border around the content, extending the size of the container in the screen but not adding additional space within the content box.  The syntax is as follows: box-sizing: border-box ; border: width type color;')
+console.log('')
+
+//10.
+console.log('10. How to change the direction of a linear-gradient ?')
+console.log('background: linear-gradient(direction, color, color, color ...)')
+console.log('background: linear-gradient(to right bottom, red, green, blue)')
+console.log('')
+
+
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+//1.
+console.log('1. ')
+console.log('')
+console.log('')
+
+//2.
+console.log('2. ')
+console.log('')
+console.log('')
+
+//3.
+console.log('3. ')
+console.log('')
+console.log('')
+
+//4.
+console.log('4. ')
+console.log('')
+console.log('')
+
+//5.
+console.log('5. ')
+console.log('')
+console.log('')
