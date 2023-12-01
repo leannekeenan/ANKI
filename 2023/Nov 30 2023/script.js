a67984a465d18178a9425e59a925b52e52c51917
Leanne Keenan <l_keenan@outlook.com>
2023-11-30 17:52:57 -0800

    Added Daily Training Session

---
+++ 2023/Nov 30 2023/script.js
@@ -0,0 +1,80 @@
+/*ANKI Review: HTML*/
+console.log('ANKI Review: HTML')
+//1.
+console.log('1. disabled boolean')
+console.log('removes user ability to action a toggle, button, or interactive element within the DOM')
+console.log('')
+
+//2.
+console.log('2. IP Address')
+console.log('Internet Protocol Address.  The numerical address that represents teh device you are making a request from so the collection of servers we refer to as the internet can return a DNS value from the provided URL to render the requested data to the user')
+console.log('')
+
+//3.
+console.log('3. <header> tag')
+console.log('meta tag to identify content that is placed at the beginning of a webpage')
+console.log('')
+
+//4.
+console.log('4. class selector')
+console.log('a identifier used to provide 010 points of specificity to a DO  element.  These identifiers can be used on multiple objects to group them together and effectively provide design or function to them with much more DRY code')
+console.log('')
+
+//5.
+console.log('5. <br>')
+console.log('break tag.  Used to add a break between HTML elements but is not recommended as this is a styling element which should be left to CSS')
+console.log('')
+
+//6.
+console.log('6. viewport maximum-scale property')
+console.log('sets the maximum valus for how large a user can scale the content of a website ti reduce diminished image quality or limited readability')
+console.log('')
+
+//7.
+console.log('7. viewport')
+console.log('meta tag value that sets the ability to zoomin and out or at all for a webpage.  Values can include userscalable meaning users can zoomin and out at will, initial-scale which starts at a set value, generally 1.0 for 100%, and maximum scale which starts at 1.0 and scales to a set zoom value to reduce diminished image quality or limited readabilit')
+console.log('')
+
+//8.
+console.log('8. viewport property options')
+console.log('initial scale which sets a starting viewport size, user-scaleable, which allows users to zoom in and out , and maximum-scale which wets the maximum zoom value of a webpage')
+console.log('')
+
+//9.
+console.log('9. legend')
+console.log('used to add titles to form fieldsets')
+console.log('')
+
+//10.
+console.log('10. target="_blank"')
+console.log('opens content into a blank window outside of the current page')
+console.log('')
+
+
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+//1.
+console.log('1. ')
+console.log('')
+console.log('')
+
+//2.
+console.log('2. ')
+console.log('')
+console.log('')
+
+//3.
+console.log('3. ')
+console.log('')
+console.log('')
+
+//4.
+console.log('4. ')
+console.log('')
+console.log('')
+
+//5.
+console.log('5. ')
+console.log('')
+console.log('')
