a55067287a61a4b3f541527791839b053490eaa3
Leanne Keenan <l_keenan@outlook.com>
2023-12-02 14:18:04 -0800

    Added Daily Training Session

---
+++ 2023/Dec 02 2023/script.js
@@ -0,0 +1,95 @@
+/*ANKI Review: JavaScript*/
+console.log('ANKI Review: JavaScript')
+//1.
+console.log('1. Spread operator')
+console.log('"...".  Used to break arrays up into individual elements and are iterated over individually instead of as a collective')
+console.log('')
+
+//2.
+console.log('2. string interpolation')
+console.log('using dynamicn values like ternary operators to link strings together')
+let str1 = 'string'
+let interpolstion = 'interpolation'
+console.log(`${str1} ${interpolstion}`)
+console.log('')
+
+//3.
+console.log('3. obj lit spread operator use')
+let obj = 'object';
+let literal = `${obj} literal`;
+console.log(literal)
+console.log('')
+
+//4.
+console.log('4. obj lit when used in a function call')
+let functionCall = () => {
+    let objlit = {
+        myName: "Leanne",
+        myOccupation: "Software Engineer"
+    }
+    console.log(` I'\m ${objlit.myName}, ${objlit.myOccupation}`)
+}
+functionCall()
+console.log('')
+console.log('')
+
+//5.
+console.log('5. spread in an array literal')
+let spread1 = [1, 2, 3];
+let spread2 = [...spread1, 4, 5, 6];
+console.log(spread2)
+console.log('')
+
+//6.
+console.log('6. querySelector()')
+console.log('Used to grab elements from the dom to manipulate using methods and properties')
+console.log('')
+
+//7.
+console.log('7. classList')
+console.log('Used to add remove or toggle classes on and off of DOM elements allowing for effects associated with teh class to be manipulated as needed')
+console.log('')
+
+//8.
+console.log('8. Document')
+console.log('Special object in JavaScript that is used to access the DOM model objects.  THe document in total is the interface for users to interact with the content of a website')
+console.log('')
+
+//9.
+console.log('9. var')
+console.log('a declaration type that specifies a function scoped variable if declared within a function. Else it is considered a globally accessable variable')
+console.log('')
+
+//10.
+console.log('10. document.body')
+console.log('Used to access dom elements within the bodt of the document from the script')
+console.log('')
+
+
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+//1.
+console.log('1. ')
+console.log('')
+console.log('')
+
+//2.
+console.log('2. ')
+console.log('')
+console.log('')
+
+//3.
+console.log('3. ')
+console.log('')
+console.log('')
+
+//4.
+console.log('4. ')
+console.log('')
+console.log('')
+
+//5.
+console.log('5. ')
+console.log('')
+console.log('')
