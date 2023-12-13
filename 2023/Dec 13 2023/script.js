e808858843009e4f1149c8a8ee2852a205a71f34
Leanne Keenan <l_keenan@outlook.com>
2023-12-13 11:40:39 -0800

    Added Daily Training Session

---
+++ 2023/Dec 13 2023/script.js
@@ -0,0 +1,240 @@
+/*ANKI Review: */
+console.log('ANKI Review:')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1.Vertical Align property'
+console.log('1. Vertical Align property')
+console.log('Used to adjust the alignment of inline, inline-block, or table cell DOM elements vertical position within its parent container.  The property accepts values top, center, and bottom or a numerical value providing a set value, percentage, or vertical height')
+
+reviewAnswer1.textContent = ' - Used to adjust the alignment of inline, inline-block, or table cell DOM elements vertical position within its parent container.  The property accepts values top, center, and bottom or a numerical value providing a set value, percentage, or vertical height'
+console.log('')
+
+
+
+
+//2.
+reviewQuestion2.textContent = '2. Responsive Design'
+console.log('2. Responsive Design')
+console.log('The process of developing a website using media queries to adjust the DOM content based on the viewport size')
+
+reviewAnswer2.textContent = ' - The process of developing a website using media queries to adjust the DOM content based on the viewport size'
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. How do you link CSS to an HTML document?'
+console.log('3. How do you link CSS to an HTML document?')
+console.log('<link rel="stylesheet" href="/style.css">')
+
+reviewAnswer3.textContent = ' - <link rel="stylesheet" href="/style.css">'
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. start attribute'
+console.log('4. start attribute')
+console.log('In the context of CSS attribute selectors, the ^= operator is used to select elements whose attribute value begins with a specified value. In the context of HTML, the start attribute is used to specify the start value of the first list item in an ordered list')
+
+reviewAnswer4.textContent = ' - In the context of CSS attribute selectors, the ^= operator is used to select elements whose attribute value begins with a specified value. In the context of HTML, the start attribute is used to specify the start value of the first list item in an ordered list'
+console.log('')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. max-height'
+console.log('5. max-height')
+console.log('sets the maximum hight of an element.  This will ensure the element will not grow larger than the set value')
+
+
+reviewAnswer5.textContent = ' - sets the maximum hight of an element.  This will ensure the element will not grow larger than the set value'
+console.log('')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. Inline element'
+console.log('6. Inline element')
+console.log('DOM elements that only take up the space neede to fit their content and will share remaing space with other inline objects.  These elements remain within the flow of the document.')
+
+reviewAnswer6.textContent = ' - DOM elements that only take up the space neede to fit their content and will share remaing space with other inline objects.  These elements remain within the flow of the document.'
+console.log('')
+
+
+
+
+//7.
+reviewQuestion7.textContent = '7. @import'
+console.log('7. @import')
+console.log('can be used in CSS to import external files to keep code clean and individually developed')
+
+reviewAnswer7.textContent = ' - can be used in CSS to import external files to keep code clean and individually developed'
+console.log('')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. When to use blockquote?'
+console.log('8. When to use blockquote?')
+console.log('Blockquotes are used for quoting large blocks of text from a previous works')
+
+reviewAnswer8.textContent = ' - Blockquotes are used for quoting large blocks of text from a previous works'
+console.log('')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. border-box value'
+console.log('9. border-box value')
+console.log('box-sizing: border-box sets the with of an object to include the additional border and pagging when included, increasing the size of a 10px box to 20px if it has a border of 5px on its left and right or 20px high if it has a border of 5px on the top and bottom')
+
+reviewAnswer9.textContent = ' - box-sizing: border-box sets the with of an object to include the additional border and pagging when included, increasing the size of a 10px box to 20px if it has a border of 5px on its left and right or 20px high if it has a border of 5px on the top and bottom'
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. list-style position property'
+console.log('10. list-style position property')
+console.log('adjusts the position and alignment of list styling bulletpoint')
+
+reviewAnswer10.textContent = ' - adjusts the position and alignment of list styling bulletpoints'
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1.'
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = ' - '
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2.'
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = ' - '
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3.'
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = ' - '
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4.'
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = ' - '
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5.'
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = ' - '
+console.log('')
+
+
+
+//codewarsQuestion6.textContent = '6.'
+//codewarsAnswer6.textContent = ' - '
+//codewarsQuestion7.textContent = '7.'
+//codewarsAnswer7.textContent = ' - '
+//codewarsQuestion8.textContent = '8.'
+//codewarsAnswer8.textContent = ' - '
+//codewarsQuestion9.textContent = '9.'
+//codewarsAnswer9.textContent = ' - '
+//codewarsQuestion10.textContent = '10.'
+//codewarsAnswer10.textContent = ' - '
+
+
+
+
+
+
+
+
+
+
