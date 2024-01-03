8606801c93c7064b42ff5674aa8cdf48b383211e
Leanne Keenan <l_keenan@outlook.com>
2024-01-03 11:45:01 -0800

    Added Daily Training Session

---
+++ 2024/01 - January/01.03.2024/script.js
@@ -0,0 +1,240 @@
+/*ANKI Review: */
+console.log('ANKI Review:')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1. SMACSS'
+console.log('1. SMACSS')
+console.log('"SMACSS" is a method of structuring CSS content in a modular mannar allowing it to be moved, copied or removed withiut altering the content of other sections of the page.  The structure includes modules, states, theme, base and layout')
+
+reviewAnswer1.textContent = ' - "SMACSS" is a method of structuring CSS content in a modular mannar allowing it to be moved, copied or removed withiut altering the content of other sections of the page.  The structure includes modules, states, theme, base and layout'
+console.log('')
+
+
+
+
+//2.
+reviewQuestion2.textContent = '2. CSS Grid'
+console.log('2. CSS Grid')
+console.log(' using rows and columns')
+
+reviewAnswer2.textContent = ' - A property of CSS allowing content to be organized in two dimensions instead of just one as flex and inline properties provide using rows and columns'
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. How do you make embedded media flelxible'
+console.log('3. How do you make embedded media flelxible')
+console.log('set the media obect within a flex object like a div and use the properties afforded to flex objects like position properties and offsets')
+
+reviewAnswer3.textContent = ' - set the media obect within a flex object like a div and use the properties afforded to flex objects like position properties and offsets'
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. What is a web browser'
+console.log('4. What is a web browser')
+console.log('A web browser is a computer program that connects a users computer to a network of servers via IP address, providing the user a link to the content of another computer or server to fulfil a request, generally enterd in the form of a URL (Uniform resource code Locator)')
+
+reviewAnswer4.textContent = ' - A web browser is a computer program that connects a users computer to a network of servers via IP address, providing the user a link to the content of another computer or server to fulfil a request, generally enterd in the form of a URL (Uniform resource code Locator)'
+console.log('')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. How to centralize design related settings'
+console.log('5. How to centralize design related settings')
+console.log('1. Use SMACSS, 2. use @import and have all of your CSS files seperate, 3. USE COMMENTS, 4. use consistant formatting in property order and class/id naming, 5. Use CSS Variable custom properties like ":root"')
+
+
+reviewAnswer5.textContent = ' - 1. Use SMACSS, 2. use @import and have all of your CSS files seperate, 3. USE COMMENTS, 4. use consistant formatting in property order and class/id naming, 5. Use CSS Variable custom properties like ":root"'
+console.log('')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. How do you measure compression'
+console.log('6. How do you measure compression')
+console.log('file size / compression rate = compression size.  This can be done using programs like compressor.io ')
+
+reviewAnswer6.textContent = ' - file size - compression rate / compression rate * 100 = compression size.  This can be done using programs like compression.io '
+console.log('')
+
+
+
+
+//7.
+reviewQuestion7.textContent = '7. What is the golden rule reguarding front end programming'
+console.log('7. What is the golden rule reguarding front end programming')
+console.log('Maintaining a seperation of concerns.  Keep content in HTML, style in CSS and function in JS')
+
+reviewAnswer7.textContent = ' - Maintaining a seperation of concerns.  Keep content in HTML, style in CSS and function in JS'
+console.log('')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. gzip compression'
+console.log('8. gzip compression')
+console.log('A data compression method used to reduce the size of files, particularly text-based files, when transmitting them over the internet. It is widely employed in web server configurations to compress HTML, CSS, JavaScript, and other text files before sending them to a web browser.')
+
+reviewAnswer8.textContent = ' - a data compression method used to reduce the file size of text based documents (although other types of files can be compressed as well)when transmitting them over the internet'
+console.log('')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. Components directory'
+console.log('9. Components directory')
+console.log('The component directory is the collection of files within a parent directory allowing the user to see the content of the parent, but keeping the components seperate and accessable based on need')
+
+reviewAnswer9.textContent = ' - The component directory is the collection of files within a parent directory allowing the user to see the content of the parent, but keeping the components seperate and accessable based on need'
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. SMACSS = State'
+console.log('10. SMACSS = State')
+console.log('The part of SMACSS that sections out objects that have a pseudo state such as nth children, hover states, active states and so on')
+
+reviewAnswer10.textContent = ' - The part of SMACSS that sections out objects that have a pseudo state such as nth children, hover states, active states and so on'
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1.'
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = ' - '
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2.'
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = ' - '
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3.'
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = ' - '
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4.'
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = ' - '
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5.'
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = ' - '
+console.log('')
+
+
+
+// codewarsQuestion6.textContent = '6.'
+// codewarsAnswer6.textContent = ' - '
+// codewarsQuestion7.textContent = '7.'
+// codewarsAnswer7.textContent = ' - '
+// codewarsQuestion8.textContent = '8.'
+// codewarsAnswer8.textContent = ' - '
+// codewarsQuestion9.textContent = '9.'
+// codewarsAnswer9.textContent = ' - '
+// codewarsQuestion10.textContent = '10.'
+// codewarsAnswer10.textContent = ' - '
+
+
+
+
+
+
+
+
+
+
