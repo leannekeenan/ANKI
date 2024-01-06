b7fef98b46f8ccb6d42ccec409dea417ff149094
Leanne Keenan <l_keenan@outlook.com>
2024-01-06 10:31:51 -0800

    Added Daily Training Session

---
+++ 2024/01. January/01.06.2024/script.js
@@ -0,0 +1,270 @@
+/*ANKI Review: */
+console.log('ANKI Review: JavaScript Review')
+let reviewQuestion0 = document.getElementById('rq0')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer0 = document.getElementById('ra0')
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion0.textContent = '#0 (because when seeing you work, he just can not help himself). Keyword "extends"'
+console.log('1. Keyword "extends"')
+console.log('Used to extend a node element from a parent and allow it to keep inherited properties.  For example if you have an animal object, you can create a dog element that extends from the animal and keeps any animal properyis from the animal parent bt can also have unique proeprties for expressly dogs as well in its own object')
+
+reviewAnswer0.textContent = ' - Used to extend a node element from a parent and allow it to keep inherited properties.  For example if you have an animal object, you can create a dog element that extends from the animal and keeps any animal properyis from the animal parent bt can also have unique proeprties for expressly dogs as well in its own object'
+console.log('')
+
+//1.
+reviewQuestion1.textContent = '1. document'
+console.log('1. document')
+console.log('keyword used to access elements on the DOM.  Document is the visual representation of the content organized in a viewport')
+
+reviewAnswer1.textContent = ' - keyword used to access elements on the DOM.  Document is the visual representation of the content organized in a viewport'
+console.log('')
+
+
+//2.
+reviewQuestion2.textContent = '2. variable++'
+console.log('2. variable++')
+console.log('variable++ increments after its current value has been used')
+
+reviewAnswer2.textContent = ' - variable++ increments after its current value has been used'
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. Final Expression'
+console.log('3. Final Expression')
+console.log('The thrid portion of a for loop stating the pre or post incrementing operation (i++/i--)')
+
+reviewAnswer3.textContent = ' - The thrid portion of a for loop stating the pre or post incrementing operation (i++/i--)'
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. Syntax for spread operator in function calls'
+console.log('4. Syntax for spread operator in function calls')
+let spreadInFunction = () => {
+    let person = {
+        name: 'Adam',
+        relation: 'husband',
+        length: 8
+    }
+    console.log(`hello, my ${person.relation} is ${person.name}. We met ${person.length} years ago.`)
+}
+spreadInFunction()
+
+reviewAnswer4.textContent = ' - '
+console.log('')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. Abstraction'
+console.log('5. Abstraction')
+console.log('')
+
+
+reviewAnswer5.textContent = ' - The showing or hiding of object details to a user'
+console.log('The showing or hiding of object details to a user')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. fetch syntax'
+console.log('6. fetch syntax')
+console.log('')
+
+reviewAnswer6.textContent = ' - '
+console.log('')
+
+function getFetch() {
+    fetch(url).then(res => {
+        if(!res.ok) {
+            throw new Error('error')
+        }
+        return res.json()
+    })
+    .then(data => {
+
+    })
+    .catch(error => {
+        return 'error', error
+    })
+        
+}
+
+
+//7.
+reviewQuestion7.textContent = '7. Let Declaration'
+console.log('7. Let Declaration')
+console.log('')
+
+reviewAnswer7.textContent = ' - Declares variables that can be reassigned'
+console.log('Declares variables that can be reassigned')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. Keyword "extends"'
+console.log('8. keyword "extends"')
+console.log('')
+
+reviewAnswer8.textContent = ' - Used to extend a node element from a parent and allow it to keep inherited properties.  For example if you have an animal object, you can create a dog element that extends from the animal and keeps any animal properyis from the animal parent bt can also have unique proeprties for expressly dogs as well in its own object'
+console.log('Used to extend a node element from a parent and allow it to keep inherited properties.  For example if you have an animal object, you can create a dog element that extends from the animal and keeps any animal properyis from the animal parent bt can also have unique proeprties for expressly dogs as well in its own object')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. Polymorphism'
+console.log('9. Polymorphism')
+console.log('an OOP aspect that allows objects to morph based on their use')
+
+reviewAnswer9.textContent = ' - an OOP aspect that allows objects to morph based on their use'
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. function declaration syntax'
+console.log('10. function declaration syntax')
+console.log('let function = (parameter) => {}')
+
+reviewAnswer10.textContent = ' - let function = (parameter) => {}'
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1.'
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = ' - '
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2.'
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = ' - '
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3.'
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = ' - '
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4.'
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = ' - '
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5.'
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = ' - '
+console.log('')
+
+
+
+codewarsQuestion6.textContent = '6.'
+codewarsAnswer6.textContent = ' - '
+codewarsQuestion7.textContent = '7.'
+codewarsAnswer7.textContent = ' - '
+codewarsQuestion8.textContent = '8.'
+codewarsAnswer8.textContent = ' - '
+codewarsQuestion9.textContent = '9.'
+codewarsAnswer9.textContent = ' - '
+codewarsQuestion10.textContent = '10.'
+codewarsAnswer10.textContent = ' - '
+
+
+
+
+
+
+
+
+
+
