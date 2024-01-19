3817e88fe85df7f87c09bfa5423ed2f251537adf
Leanne Keenan <l_keenan@outlook.com>
2024-01-19 11:54:30 -0800

    Added Daily Training Session

---
+++ 2024/01. January/01.19.2024/script.js
@@ -0,0 +1,240 @@
+/*ANKI Review: */
+console.log('ANKI Review:')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1. <footer> tag'
+console.log('1.  <footer> tag')
+console.log('Semantic HTML tag identifying content that will be positioned at the end of a document')
+
+reviewAnswer1.textContent = ' - Semantic HTML tag identifying content that will be positioned at the end of a document'
+console.log('')
+
+
+
+
+//2.
+reviewQuestion2.textContent = '2. How to target HTML elements by their attributes?'
+console.log('2. How to target HTML elements by their attributes?')
+console.log('Use bracket notation and the assignment operator. [attribute="value"]')
+
+reviewAnswer2.textContent = ' - Use bracket notation and the assignment operator. [attribute="value"]'
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. <fieldset>'
+console.log('3. <fieldset>')
+console.log('Semantic tag used to create sections in forms')
+
+reviewAnswer3.textContent = ' - Semantic tag used to create sections in forms'
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. src=""'
+console.log('4. src=""')
+console.log('source tag.  used to link sourced media content')
+
+reviewAnswer4.textContent = ' - source tag.  used to link sourced media content'
+console.log('')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. validator'
+console.log('5. validator')
+console.log('An internal functionality program within developer tools used to check for syntax errors in code')
+
+
+reviewAnswer5.textContent = ' - An internal functionality program within developer tools used to check for syntax errors in code'
+console.log('')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. <caption>'
+console.log('6. <caption>')
+console.log('A semantic HTML tag used to provide captions to content within a table')
+
+reviewAnswer6.textContent = ' - A semantic HTML tag used to provide captions to content within a table'
+console.log('')
+
+
+
+
+//7.
+reviewQuestion7.textContent = '7. Octothorp'
+console.log('7. Octothorp')
+console.log('"#" is used in HTML to identify object id\'s')
+
+reviewAnswer7.textContent = ' - "#" is used in HTML to identify object id\'s'
+console.log('')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. Rowspan'
+console.log('8. Rowspan')
+console.log('Attribute of tables that can allow the cells of a table to span the height of multiple rows')
+
+reviewAnswer8.textContent = ' - Attribute of tables that can allow the cells of a table to span the height of multiple rows'
+console.log('')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. data-* attribute'
+console.log('9. data-* attribute')
+console.log('Stores custom data on HTML elements')
+
+reviewAnswer9.textContent = ' - Stores custom data on HTML elements'
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. value'
+console.log('10. value')
+console.log('Can be added to a HTML form elements and used as a representitive value of the element in the DOM')
+
+reviewAnswer10.textContent = ' - Can be added to a HTML form elements and used as a representitive value of the element in the DOM'
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1.'
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = ' - '
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2.'
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = ' - '
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3.'
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = ' - '
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4.'
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = ' - '
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5.'
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = ' - '
+console.log('')
+
+
+
+// codewarsQuestion6.textContent = '6.'
+// codewarsAnswer6.textContent = ' - '
+// codewarsQuestion7.textContent = '7.'
+// codewarsAnswer7.textContent = ' - '
+// codewarsQuestion8.textContent = '8.'
+// codewarsAnswer8.textContent = ' - '
+// codewarsQuestion9.textContent = '9.'
+// codewarsAnswer9.textContent = ' - '
+// codewarsQuestion10.textContent = '10.'
+// codewarsAnswer10.textContent = ' - '
+
+
+
+
+
+
+
+
+
+
