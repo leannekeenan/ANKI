d4bc45d4b80cb8565ff452fdf66b4cb47fad612c
Leanne Keenan <l_keenan@outlook.com>
2024-01-21 10:13:57 -0800

    Added Daily Training Session

---
+++ 2024/01. January/01.21.2024/script.js
@@ -0,0 +1,235 @@
+/*ANKI Review: */
+console.log('ANKI Review: CSS')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1. What is the formula for finding the total height of an element?'
+console.log('1. What is the formula for finding the total height of an element?')
+console.log('')
+
+reviewAnswer1.textContent = ' - top-margin + top-border + top-padding + content + bottom-padding + bottom-border + bottom-margin'
+console.log('top-margin + top-border + top-padding + content + bottom-padding + bottom-border + bottom-margin')
+
+
+
+
+//2.
+reviewQuestion2.textContent = '2. Relative Length Units'
+console.log('2. Relative Length Units')
+console.log('1, Percentages, 2.rem, 3. em, 4. vh/vw, ')
+
+reviewAnswer2.textContent = ' - 1, Percentages, 2.rem, 3. em, 4. vh/vw, '
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. Vertical Align property'
+console.log('3. Vertical Align property')
+console.log('CSS property used to set vertical alignment of inline, inline-block, and table cell elements')
+
+reviewAnswer3.textContent = ' - CSS property used to set vertical alignment of inline, inline-block, and table cell elements'
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. "clear" property'
+console.log('4. "clear" property')
+console.log('')
+
+reviewAnswer4.textContent = ' - Used to remove the effects of the float property an ccan accept left, right, none or both as values'
+console.log('Used to remove the effects of the float property an ccan accept left, right, none or both as values')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. display: block'
+console.log('5. display: block')
+console.log('A CSS property used to ensure objects will stack atop each other rather than siting next to each other.  ')
+
+
+reviewAnswer5.textContent = ' - A CSS property used to ensure objects will stack atop each other rather than siting next to each other.  '
+console.log('')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. What is the padding-box?'
+console.log('6. What is the padding-box?')
+console.log('a value that can be used with the property box-sizing to set the sizing parameters of DOM elements to include the content, but not the padding, border, or margin')
+
+reviewAnswer6.textContent = ' - a value that can be used with the property box-sizing to set the sizing parameters of DOM elements to include the content, but not the padding, border, or margin'
+//7.
+reviewQuestion7.textContent = '7. Media Queries & Types'
+console.log('7. Media Queries & Types')
+console.log('Media queries allow for the content of a page to be reorganized to fit a set viewport screen size.  This allows websites to be responsive.  Types of media queries include writing them directly in your CSS or using an "@import" tag to iport a seperate file with the desired content.')
+
+reviewAnswer7.textContent = ' - Media queries allow for the content of a page to be reorganized to fit a set viewport screen size.  This allows websites to be responsive.  Types of media queries include writing them directly in your CSS or using an "@import" tag to iport a seperate file with the desired content.'
+console.log('')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. box-sizing'
+console.log('8. box-sizing')
+console.log('CSS property that can determine if the border, padding or margin associated with an element will be considered a part of its dimentions')
+
+reviewAnswer8.textContent = ' - CSS property that can determine if the border, padding or margin associated with an element will be considered a part of its dimentions'
+console.log('')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. @media'
+console.log('9. @media')
+console.log('syntax for media queries')
+
+reviewAnswer9.textContent = ' - syntax for media queries'
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. What is the static position property mean?'
+console.log('10. What is the static position property mean?')
+console.log('The static position is the startiong position of an object before any CSS is applied')
+
+reviewAnswer10.textContent = ' - The static position is the startiong position of an object before any CSS is applied'
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1.'
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = ' - '
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2.'
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = ' - '
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3.'
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = ' - '
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4.'
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = ' - '
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5.'
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = ' - '
+console.log('')
+
+
+
+// codewarsQuestion6.textContent = '6.'
+// codewarsAnswer6.textContent = ' - '
+// codewarsQuestion7.textContent = '7.'
+// codewarsAnswer7.textContent = ' - '
+// codewarsQuestion8.textContent = '8.'
+// codewarsAnswer8.textContent = ' - '
+// codewarsQuestion9.textContent = '9.'
+// codewarsAnswer9.textContent = ' - '
+// codewarsQuestion10.textContent = '10.'
+// codewarsAnswer10.textContent = ' - '
+
+
+
+
+
+
+
+
+
+
