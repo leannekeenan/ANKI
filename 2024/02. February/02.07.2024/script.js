43b9739d2f319ae286e5a9914ed5cc0396979ffe
Leanne Keenan <l_keenan@outlook.com>
2024-02-07 13:58:29 -0800

    Added Daily Training Session

---
+++ 2024/02. February/02.07.2024/script.js
@@ -0,0 +1,240 @@
+/*ANKI Review: */
+console.log('ANKI Review: General Knowledge')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1. CSS Grid'
+console.log('1. CSS Grid')
+console.log('CSS property used to arrange content in two dimentions (vertical and horizontal)')
+
+reviewAnswer1.textContent = `- CSS property used to arrange content in two dimentions (vertical and horizontal)`
+console.log('')
+
+
+
+
+//2.
+reviewQuestion2.textContent = '2. Name 4 ways to make embedded media flexible'
+console.log('2. Name 4 ways to make embedded media flexible')
+console.log('1. parent container percentage restraint - 2. "ojbect-fit" property - 3. set width to 100% of contqiner and height to auto - 4. use the "sizes" attribute with elements that can accept the "srcset" attribute')
+
+reviewAnswer2.textContent = `- 1. parent container percentage restraint - 2. "ojbect-fit" property - 3. set width to 100% of contqiner and height to auto - 4. use the "sizes" attribute with elements that can accept the "srcset" attribute`
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. What happens when a computer makes a request for a web page?'
+console.log('3. What happens when a computer makes a request for a web page?')
+console.log('Your local network IP address searches for a URL typed into a search bar, which itself is filtered through a DNS to determine the "computer translation" of the URL. Once the content IP has been found in the DNS, the data is then erturned to yut local neteork host ')
+
+reviewAnswer3.textContent = `- Your local network IP address searches for a URL typed into a search bar, which itself is filtered through a DNS to determine the "computer translation" of the URL. Once the content IP has been found in the DNS, the data is then erturned to yut local neteork host `
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. What is a web browser'
+console.log('4. What is a web browser')
+console.log('A computer program used to access the internet from a user device')
+
+reviewAnswer4.textContent = `- A computer program used to access the internet from a user device`
+console.log('')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. Scaleable Modular Architecture'
+console.log('5. Scaleable Modular Architecture')
+console.log('A organizational process of writing CSS code in a manner that allows it to be easier to read and divided into subjective categories such as modules, themes, states, defaults, and layout')
+
+
+reviewAnswer5.textContent = `- A organizational process of writing CSS code in a manner that allows it to be easier to read and divided into subjective categories such as modules, themes, states, defaults, and layout`
+console.log('')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. SMACSS: State'
+console.log('6. SMACSS: State')
+console.log('A segment of the SMACSS process that seperates CSS related to the state an object is in at any given time such as hover states, click states and active states')
+
+reviewAnswer6.textContent = `- A segment of the SMACSS process that seperates CSS related to the state an object is in at any given time such as hover states, click states and active states`
+console.log('')
+
+
+
+
+//7.
+reviewQuestion7.textContent = '7. gzip compression'
+console.log('7. gzip compression')
+console.log('GNU zip compression is a file compression algorithm used to reduce the size of files, particularly text based files, when transfering the content')
+
+reviewAnswer7.textContent = `- GNU zip compression is a file compression algorithm used to reduce the size of files, particularly text based files, when transfering the content`
+console.log('')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. What is a markup language'
+console.log('8. What is a markup language')
+console.log('A coding language that renders content directlyto the DOM without manipulation')
+
+reviewAnswer8.textContent = `- A coding language that renders content directly to the DOM without manipulation`
+console.log('')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. how to measure image compression?'
+console.log('9. how to measure image compression?')
+console.log('')
+
+reviewAnswer9.textContent = `- `
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. Adaptive Web Design'
+console.log('10. Adaptive Web Design')
+console.log('A way of building websites that has developers build individual concept pages for each possible breakpoint for a screen size rather than using responsive design')
+
+reviewAnswer10.textContent = `- A way of building websites that has developers build individual concept pages for each possible breakpoint for a screen size rather than using responsive design`
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1. '
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = `- `
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2. '
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = `- `
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3. '
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = `- `
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4. '
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = `- `
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5. '
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = `- `
+console.log('')
+
+
+
+// codewarsQuestion6.textContent = '6.'
+// codewarsAnswer6.textContent = `- `
+// codewarsQuestion7.textContent = '7.'
+// codewarsAnswer7.textContent = `- `
+// codewarsQuestion8.textContent = '8.'
+// codewarsAnswer8.textContent = `- `
+// codewarsQuestion9.textContent = '9.'
+// codewarsAnswer9.textContent = `- `
+// codewarsQuestion10.textContent = '10.'
+// codewarsAnswer10.textContent = `- `
+
+
+
+
+
+
+
+
+
+
