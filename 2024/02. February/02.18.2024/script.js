695bb52215bd726cd9aa8776d9141a936c78cf6c
Leanne Keenan <l_keenan@outlook.com>
2024-02-18 11:10:58 -0800

    Added Daily Training Session

---
+++ 2024/02. February/02.18.2024/script.js
@@ -0,0 +1,240 @@
+/*ANKI Review: */
+console.log('ANKI Review: JavaScript DOM Manipulation Method Review')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1. '
+console.log('1. ')
+console.log('')
+
+reviewAnswer1.textContent = `- `
+console.log('')
+
+
+
+
+//2.
+reviewQuestion2.textContent = '2. '
+console.log('2. ')
+console.log('')
+
+reviewAnswer2.textContent = `- `
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. '
+console.log('3. ')
+console.log('')
+
+reviewAnswer3.textContent = `- `
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. '
+console.log('4. ')
+console.log('')
+
+reviewAnswer4.textContent = `- `
+console.log('')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. '
+console.log('5. ')
+console.log('')
+
+
+reviewAnswer5.textContent = `- `
+console.log('')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. '
+console.log('6. ')
+console.log('')
+
+reviewAnswer6.textContent = `- `
+console.log('')
+
+
+
+
+//7.
+reviewQuestion7.textContent = '7. '
+console.log('7. ')
+console.log('')
+
+reviewAnswer7.textContent = `- `
+console.log('')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. '
+console.log('8. ')
+console.log('')
+
+reviewAnswer8.textContent = `- `
+console.log('')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. '
+console.log('9. ')
+console.log('')
+
+reviewAnswer9.textContent = `- `
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. '
+console.log('10. ')
+console.log('')
+
+reviewAnswer10.textContent = `- `
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1. '
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = `- `
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2. '
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = `- `
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3. '
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = `- `
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4. '
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = `- `
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5. '
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = `- `
+console.log('')
+
+
+
+// codewarsQuestion6.textContent = '6.'
+// codewarsAnswer6.textContent = `- `
+// codewarsQuestion7.textContent = '7.'
+// codewarsAnswer7.textContent = `- `
+// codewarsQuestion8.textContent = '8.'
+// codewarsAnswer8.textContent = `- `
+// codewarsQuestion9.textContent = '9.'
+// codewarsAnswer9.textContent = `- `
+// codewarsQuestion10.textContent = '10.'
+// codewarsAnswer10.textContent = `- `
+
+
+
+
+
+
+
+
+
+
