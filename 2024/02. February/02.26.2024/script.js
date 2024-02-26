0632d7feab2624d57c4d375f779a03d2c67df1bd
Leanne Keenan <l_keenan@outlook.com>
2024-02-26 12:42:05 -0800

    Added Daily Training Session

--- 2024/02. February/02.26.2024/script.js
+++ 2024/02. February/02.26.2024/script.js
@@ -1,5 +1,5 @@
 /*ANKI Review: */
-console.log('ANKI Review:')
+console.log('ANKI Review: HTML Terms')
 let reviewQuestion1 = document.getElementById('rq1')
 let reviewQuestion2 = document.getElementById('rq2')
 let reviewQuestion3 = document.getElementById('rq3')
@@ -27,22 +27,22 @@ let reviewAnswer10 = document.getElementById('ra10')
 
 
 //1.
-reviewQuestion1.textContent = '1. '
-console.log('1. ')
-console.log('')
+reviewQuestion1.textContent = '1. type attribute'
+console.log('1. type attribute')
+console.log('in HTML, the type atttribute is assigned to form elements to inform the user what information is being asked in that input such as email, phone numbers, text, numerical values and more')
 
-reviewAnswer1.textContent = `- `
+reviewAnswer1.textContent = `- in HTML, the type atttribute is assigned to form elements to inform the user what information is being asked in that input such as email, phone numbers, text, numerical values and more`
 console.log('')
 
 
 
 
 //2.
-reviewQuestion2.textContent = '2. '
-console.log('2. ')
-console.log('')
+reviewQuestion2.textContent = '2. Fundamental differences between <section>, <article>, and <div>'
+console.log('2. Fundamental differences between <section>, <article>, and <div>')
+console.log('Semantics.  The div container is a block element with no specific designation - it is universal, and due to its lack of individuality, can make reading HTML harder to find specific page elements.  The article tag is a block level tag that specifies that the content may or may not be related to the main content of the page and can generally be seen used as a replacement for the truncated <aside> tag for things like ads and ')
 
-reviewAnswer2.textContent = `- `
+reviewAnswer2.textContent = `- Semantics.  The div container is a block element with no specific designation - it is universal, and due to its lack of individuality, can make reading HTML harder to find specific page elements.  The article tag is a block level tag that specifies that the content may or may not be related to the main content of the page and can generally be seen used as a replacement for the truncated <aside> and <sidebar> tag for things like ads.  The <section> tag is a similar tyo the <div> tag but is used to seperate content on a page by relative content, grouping like or related content into the same section.`
 console.log('')
 
 
@@ -50,89 +50,89 @@ console.log('')
 
 
 //3.
-reviewQuestion3.textContent = '3. '
-console.log('3. ')
-console.log('')
+reviewQuestion3.textContent = '3. SVC'
+console.log('3. SVC')
+console.log('Scaleable Video Content - A format that allow video aspect ratio to remain in tact reguardless of screen size')
 
-reviewAnswer3.textContent = `- `
+reviewAnswer3.textContent = `- Scaleable Video Content - A format that allow video aspect ratio to remain in tact reguardless of screen size`
 console.log('')
 
 
 
 
 //4.
-reviewQuestion4.textContent = '4. '
-console.log('4. ')
-console.log('')
+reviewQuestion4.textContent = '4. <caption>'
+console.log('4. <caption>')
+console.log('A tag used to provide detail information about an element within a table')
 
-reviewAnswer4.textContent = `- `
+reviewAnswer4.textContent = `- A tag used to provide detail information about an element within a table`
 console.log('')
 
 
 
 
 //5.
-reviewQuestion5.textContent = '5. '
-console.log('5. ')
-console.log('')
+reviewQuestion5.textContent = '5. What is the footer tag for?'
+console.log('5. What is the footer tag for?')
+console.log('The footer tag is meant to identify content that is nmeant to be positioned at the end of a webpage')
 
 
-reviewAnswer5.textContent = `- `
+reviewAnswer5.textContent = `- The footer tag is meant to identify content that is nmeant to be positioned at the end of a webpage`
 console.log('')
 
 
 
 
 //6.
-reviewQuestion6.textContent = '6. '
-console.log('6. ')
-console.log('')
+reviewQuestion6.textContent = '6. viewport property scale options'
+console.log('6. viewport property scale options')
+console.log('initial-scale specifies initial zoom level when the page first loads, user-scaleable specifies if the user can or can not zoom into the page, maximum-scale specifies the max scale for zooming in on a webpage')
 
-reviewAnswer6.textContent = `- `
+reviewAnswer6.textContent = `- initial-scale specifies initial zoom level when the page first loads, user-scaleable specifies if the user can or can not zoom into the page, maximum-scale specifies the max scale for zooming in on a webpage`
 console.log('')
 
 
 
 
 //7.
-reviewQuestion7.textContent = '7. '
-console.log('7. ')
-console.log('')
+reviewQuestion7.textContent = '7. viewport meta tag'
+console.log('7. viewport meta tag')
+console.log('<meta name="viewport" content="width=device-width,initial-scale=1.0>"')
 
-reviewAnswer7.textContent = `- `
+reviewAnswer7.textContent = `- <meta name="viewport" content="width=device-width initial-scale=1.0>`
 console.log('')
 
 
 
 
 //8.
-reviewQuestion8.textContent = '8. '
-console.log('8. ')
-console.log('')
+reviewQuestion8.textContent = '8. <legend>'
+console.log('8. <legend>')
+console.log(' HTML tag used in fieldsets to provide a key for what colors and symbols mean')
 
-reviewAnswer8.textContent = `- `
+reviewAnswer8.textContent = `-  HTML tag used in fieldsets to provide a key for what colors and symbols mean`
 console.log('')
 
 
 
 
 //9.
-reviewQuestion9.textContent = '9. '
-console.log('9. ')
-console.log('')
+reviewQuestion9.textContent = '9. data-* attribute'
+console.log('9. data-* attribute')
+console.log('This is the syntax to create a unique attribute data class for an HTMl element which can in turn be used to represent the element in JS functions and CSS manipulation')
 
-reviewAnswer9.textContent = `- `
+reviewAnswer9.textContent = `- This is the syntax to create a unique attribute data class for an HTMl element which can in turn be used to represent the element in JS functions and CSS manipulation`
 console.log('')
 
 
 
 
 //10.
-reviewQuestion10.textContent = '10. '
-console.log('10. ')
-console.log('')
+reviewQuestion10.textContent = '10. Validator'
+console.log('10. Validator')
+console.log(' and check compliance')
 
-reviewAnswer10.textContent = `- `
+reviewAnswer10.textContent = `- Internal Dev Tool used to return errors in code and check compliance`
 console.log('')
 
 
