5fe06b5cf93a808921e7611d4bd5c0cce671ba27
Leanne Keenan <l_keenan@outlook.com>
2024-03-04 18:01:12 -0800

    Added Daily Training Session

---
+++ 2024/03. March/03.04.2024/script.js
@@ -0,0 +1,201 @@
+/*ANKI Review: */
+console.log('ANKI Review: Behavioral Interview Questions')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1. A. Describe a situation in which you experienced difficulty in getting others to accept your ideas? B. What was your approach? C. How did this work? D. Were you able to successfully persuade someone to see things your way'
+reviewAnswer1.textContent = `- While working for Allied Universal, I felt it would benefit my team, and perhaps the company to switch from pen and paper timesheets to an app that could be on officer and supervisors phones, or used at a terminal laptop within their assigned campus to digitally clock in and out, as well as manage break times too. The officers of the team thought it was a fantastic idea, but I could not gain approval from management to change the current system because they feared it would caouse a security issue, or perhaps even more issues with managing time sheets than we currently were experienceing. I took the liberty of creating a prototype called C.L.I.P and began working on it as a personal project, because I believed it was still a good idea, even if it was not going to be applied for its indended use, it would be a good learning experience. Currently the app allows users to have an account where they can login in and out for the day, change campuses, and I will soon be adding the features for campus metrics, TOR submissions, previewing schedules and other features I though would be a benefit to my previous team.`
+console.log('')
+
+
+//2.
+reviewQuestion2.textContent = '2. Describe a situation in which you felt you had not communicated well enough. What did you do? How did you handle it?'
+reviewAnswer2.textContent = `- I play a bi-weeky D&D game with a few friends and when we first started the game, I think I may have thrown a monkey wrench in the game by not expressly communicating certain aspects of my character, or how I personally play the game. I felt as the last player to be added to the team I should try to be more of a support character for the current party. I tried to play as a character that was flexible with the whims of the party, but without realizing it I was giving my GM too much autonomy with what to do with my character that she felt I as a character and player were becoming apathetic to the game she had created and was under the impression I did not want to participate. The both of us decided to meet and have a discussion where we both got the opportuniity to explain our viewpoints and came to a agreement that how I was participating was in no way a reflection on her or her game, but rather me getting to evaluate the game while playing in a manner that does not impeede the actions of other players or the DM. We decided to make a few alterations to the backstory of my character and how they interact with the world allowing for a more interactive gaming experience for myself , the GM and the other players.`
+console.log('')
+
+
+//3.
+reviewQuestion3.textContent = '3. Describe a situation in which you worked diligently on a project and it did not produce the desired results. Why didn\'t you get the desired results? What did you learn from the experience?'
+reviewAnswer3.textContent = `- I was working on an App called C.L.I.P as a solution for my previous employer's time tracking system. It was intended to allow officers to clock in and out of shifts and breaks, view upcoming schedules, and other features. However, I encountered a significant issue with data retrieval from the DOM, particularly in storing time stamps accurately. Despite my efforts, the time stamps would revert to the index value of the order in which the cells were provided data upon page refresh. This technical challenge hindered my ability to produce a functioning prototype to present to my team.
+
+Reflecting on this experience, I realize that I may have underestimated the complexity of the task and lacked a comprehensive understanding of the requirements. Additionally, I learned valuable lessons about the importance of thorough testing and debugging processes, as well as the intricacies of data management within web applications.
+
+Although the project did not yield the desired results, it provided me with valuable insights into local storage mechanisms and user accessibility considerations. Moving forward, I will apply these lessons to future projects, ensuring a more robust and informed approach to development.`
+console.log('')
+
+
+//4.
+reviewQuestion4.textContent = '4. Tell me about yourself'
+reviewAnswer4.textContent = `- I'\m a front end developer with a background in customer service and a passion for user interface design. I have a degree in information technology from the university of phoenix and am the founder of Keenan Web Services, a botique front end developer company based in Redwood City focused on collaborating with local business to promote their online presence. I have also started a passion project called The Sweet Adventures Club, which is a D&D themed lifestyle brand and specialty bakery. 
+
+Through my experience I have successfully delivered solutions to a variety of clients, honing my ability to understand and address their unique needs.  These experiences have equiped me with a strong mindset and a proavtive approach to problem solving, which I believe will be avalueable asset in reaching your compainies goals.
+`
+console.log('')
+
+
+//5.
+reviewQuestion5.textContent = '5. Tell me about a situation when you were responsible for project planning. Did everything go according to your plan? If not, then why and what kind of counteractions did you have to take?'
+reviewAnswer5.textContent = `- "As a security lead, I often had to create operational plans and organize officer formations with limited resources. A common challenge was being asked to grant specialty access without clear mission parameters until the event began, sometimes conflicting with established regulations.
+
+In these situations, we relied on our training motto: 'trust but verify.' We prioritized security by scrutinizing access requests, even if it meant facing backlash later. This approach ensured we maintained safety standards while accommodating evolving circumstances.`
+console.log('')
+
+
+// //6.
+// reviewQuestion6.textContent = '6. '
+// reviewAnswer6.textContent = `- `
+// console.log('')
+
+
+// //7.
+// reviewQuestion7.textContent = '7. '
+// reviewAnswer7.textContent = `- `
+// console.log('')
+
+
+// //8.
+// reviewQuestion8.textContent = '8. '
+// reviewAnswer8.textContent = `- `
+// console.log('')
+
+
+// //9.
+// reviewQuestion9.textContent = '9. '
+// reviewAnswer9.textContent = `- `
+// console.log('')
+
+
+
+
+// //10.
+// reviewQuestion10.textContent = '10. '
+// reviewAnswer10.textContent = `- `
+// console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1. '
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = `- `
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2. '
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = `- `
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3. '
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = `- `
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4. '
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = `- `
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5. '
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = `- `
+console.log('')
+
+
+
+// codewarsQuestion6.textContent = '6.'
+// codewarsAnswer6.textContent = `- `
+// codewarsQuestion7.textContent = '7.'
+// codewarsAnswer7.textContent = `- `
+// codewarsQuestion8.textContent = '8.'
+// codewarsAnswer8.textContent = `- `
+// codewarsQuestion9.textContent = '9.'
+// codewarsAnswer9.textContent = `- `
+// codewarsQuestion10.textContent = '10.'
+// codewarsAnswer10.textContent = `- `
+
+
+
+
+
+
+
+
+
+
