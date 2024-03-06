755daa039f5bc57ea3574a773a237664819b0a08
Leanne Keenan <l_keenan@outlook.com>
2024-03-06 13:38:17 -0800

    Added Daily Training Session

---
+++ 2024/03. March/03.06.2024/script.js
@@ -0,0 +1,240 @@
+/*ANKI Review: */
+console.log('ANKI Review: General Knowledge')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1. Name 4 ways to make embedded media flexible'
+console.log('1. Name 4 ways to make embedded media flexible')
+console.log('use position: relative and setting the inset property to 0, set object to be a block element and its height and width to 100%, use object fit porperty, set margin to 0 auto and height to 100%')
+
+reviewAnswer1.textContent = `- use position: relative and setting the inset property to 0, set object to be a block element and its height and width to 100%, use object fit porperty, set margin to 0 auto and height to 100%`
+console.log('')
+
+
+
+
+//2.
+reviewQuestion2.textContent = '2. How do you measure image compression'
+console.log('2. How do you measure image compression')
+console.log('use third party products like a CDN or a build in tool llike task runner or an online tool like compression.io.  The formula is object size - compression size / file size * 100')
+
+reviewAnswer2.textContent = `- use third party products like a CDN or a build in tool llike task runner or an online tool like compression.io.  The formula is object size - compression size / file size * 100`
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. What is a markup language'
+console.log('3. What is a markup language')
+console.log('A set of rules and syntax used to structure text and media in a document that can be accessed through a web browser or computer program.')
+
+reviewAnswer3.textContent = `- A set of rules and syntax used to structure text and media in a document that can be accessed through a web browser or computer program.`
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. CSS Grid'
+console.log('4. CSS Grid')
+console.log('A CSS proerty used to organized content in a 2-dimentional format following x and y axis')
+
+reviewAnswer4.textContent = `- A CSS proerty used to organized content in a 2-dimentional format following x and y axis`
+console.log('')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. gzip compression'
+console.log('5. gzip compression')
+console.log('used to compress the file size of content while it is being shared bwtween browsers')
+
+
+reviewAnswer5.textContent = `- used to compress the file size of content while it is being shared bwtween browsers`
+console.log('')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. Benefits of short selectors'
+console.log('6. Benefits of short selectors')
+console.log('Maakes code easier to read | makes properties easier to find | simplifies reference to the selector | Makes for better inheritance | makes code more efficient')
+
+reviewAnswer6.textContent = `- Maakes code easier to read | makes properties easier to find | simplifies reference to the selector | Makes for better inheritance | makes code more efficient`
+console.log('')
+
+
+
+
+//7.
+reviewQuestion7.textContent = '7. Common preformance pitfalls in web dev'
+console.log('7. Common preformance pitfalls in web dev')
+console.log('unoptimized images, render-blocking JavaScript, a lack of catch clauses, excessive HTTP requests and frameworks, poorly organized code')
+
+reviewAnswer7.textContent = `- unoptimized images, render-blocking JavaScript, a lack of catch clauses, excessive HTTP requests and frameworks, poorly organized code`
+console.log('')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. SMACSS: Module'
+console.log('8. SMACSS: Module')
+console.log('A portion of the SMACSS organizational process that relates with elements in the DOM that are stste, theme, base, layout')
+
+reviewAnswer8.textContent = `- A portion of the SMACSS organizational process that relates with elements in the DOM that have unique selector types`
+console.log('')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. What is the golden rule of front end development'
+console.log('9. What is the golden rule of front end development')
+console.log('The seperation of concerns.  Heeping content in HTL, styling in CSS, and functionality in JavaScript')
+
+reviewAnswer9.textContent = `- The seperation of concerns.  Heeping content in HTL, styling in CSS, and functionality in JavaScript`
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. SMACSS: Base'
+console.log('10. SMACSS: Base')
+console.log('A Portion of the SMACSS development structure relating to the organizing of the pre-existing HTML selector types ')
+
+reviewAnswer10.textContent = `- A Portion of the SMACSS development structure relating to the organizing of the pre-existing HTML selector types `
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1. '
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = `- `
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2. '
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = `- `
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3. '
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = `- `
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4. '
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = `- `
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5. '
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = `- `
+console.log('')
+
+
+
+// codewarsQuestion6.textContent = '6.'
+// codewarsAnswer6.textContent = `- `
+// codewarsQuestion7.textContent = '7.'
+// codewarsAnswer7.textContent = `- `
+// codewarsQuestion8.textContent = '8.'
+// codewarsAnswer8.textContent = `- `
+// codewarsQuestion9.textContent = '9.'
+// codewarsAnswer9.textContent = `- `
+// codewarsQuestion10.textContent = '10.'
+// codewarsAnswer10.textContent = `- `
+
+
+
+
+
+
+
+
+
+
