ddc9f0c3a396288852438d9cf114bba10e5b50ea
Leanne Keenan <l_keenan@outlook.com>
2024-03-09 11:28:24 -0800

    Added Daily Training Session

--- 2024/03. March/03.09.2024/script.js
+++ 2024/03. March/03.09.2024/script.js
@@ -27,22 +27,48 @@ let reviewAnswer10 = document.getElementById('ra10')
 
 
 //1.
-reviewQuestion1.textContent = '1. '
-console.log('1. ')
-console.log('')
+reviewQuestion1.textContent = '1. fetch syntax'
+console.log('1. fetch syntax')
+console.log(`
+//define a HTML element that the event listener will be attached to
 
-reviewAnswer1.textContent = `- `
+fetch(url).then(res => {
+    if(!res.ok) {
+        throw new Error('this is a new error')
+    }
+    else {
+        return res.json()
+    }
+}).then(data => {
+    //return the pulled data from the API here
+}).catch(error => {
+ console.log('Error', error.message)
+})
+`)
+
+reviewAnswer1.textContent = ` 
+//define a HTML element that the event listener will be attached to
+
+fetch(url).then(res => {
+    if(!res.ok) {
+        return new error('this is a new error')
+    }
+    else {
+        return res.json()
+    }
+})
+`
 console.log('')
 
 
 
 
 //2.
-reviewQuestion2.textContent = '2. '
-console.log('2. ')
-console.log('')
+reviewQuestion2.textContent = '2. DOM'
+console.log('2. DOM')
+console.log('Stands for Document Object Model.  It is a modeling of the content of a website that can be manipulated without altering code of the document file')
 
-reviewAnswer2.textContent = `- `
+reviewAnswer2.textContent = `- Stands for Document Object Model.  It is a modeling of the content of a website that can be manipulated without altering code of the document file`
 console.log('')
 
 
@@ -50,89 +76,111 @@ console.log('')
 
 
 //3.
-reviewQuestion3.textContent = '3. '
-console.log('3. ')
-console.log('')
+reviewQuestion3.textContent = '3. Abstraction'
+console.log('3. Abstraction')
+console.log('A principle of teh OOP development process that allows objects to hide and show content at its discression and needs of the user. Tihs can be done my including or excluding variables from function code blocks, allowing them to be accessed by specific segment sof the progeam rather tha the entire program')
 
-reviewAnswer3.textContent = `- `
+reviewAnswer3.textContent = `- A principle of teh OOP development process that allows objects to hide and show content at its discression and needs of the user. Tihs can be done my including or excluding variables from function code blocks, allowing them to be accessed by specific segment sof the progeam rather tha the entire program`
 console.log('')
 
 
 
 
 //4.
-reviewQuestion4.textContent = '4. '
-console.log('4. ')
-console.log('')
+reviewQuestion4.textContent = '4. addEventListener'
+console.log('4. addEventListener')
+console.log('A JavaScript method used to add an event to an HTML element, allowing for a functon process to affect the nature of teh HTML ele,ent ')
 
-reviewAnswer4.textContent = `- `
+reviewAnswer4.textContent = `- A JavaScript method used to add an event to an HTML element, allowing for a functon process to affect the nature of the HTML element.`
 console.log('')
 
 
 
 
 //5.
-reviewQuestion5.textContent = '5. '
-console.log('5. ')
-console.log('')
+reviewQuestion5.textContent = '5. Polymprphism'
+console.log('5. Polymprphism')
+console.log('A principle of OOP allowing objects with different classes to be treated as objects of the same superclass. Often achieved through inheritance using the extends keyword to create subclasses that inherit from a superclass.')
 
 
-reviewAnswer5.textContent = `- `
+reviewAnswer5.textContent = `- A principle of OOP allowing objects with different classes to be treated as objects of the same superclass. Often achieved through inheritance using the extends keyword to create subclasses that inherit from a superclass.`
 console.log('')
 
 
 
 
 //6.
-reviewQuestion6.textContent = '6. '
-console.log('6. ')
-console.log('')
+reviewQuestion6.textContent = '6. keyword "extends"'
+console.log('6. keyword "extends"')
+console.log('Keyword Used in OOP to create a subclass of a superclass, allowing the sub to gain properties from its superclass, reguardless of its class name or type')
 
-reviewAnswer6.textContent = `- `
+reviewAnswer6.textContent = `- Keyword Used in OOP to create a subclass of a superclass, allowing the sub to gain properties from its superclass, reguardless of its class name or type`
 console.log('')
 
 
 
 
 //7.
-reviewQuestion7.textContent = '7. '
-console.log('7. ')
-console.log('')
+reviewQuestion7.textContent = '7. document.body'
+console.log('7. document.body')
+console.log('JavaScript properry that refers to the body element of the current HTML document')
 
-reviewAnswer7.textContent = `- `
+reviewAnswer7.textContent = `- JavaScript properry that refers to the body element of the current HTML document`
 console.log('')
 
 
 
 
 //8.
-reviewQuestion8.textContent = '8. '
-console.log('8. ')
-console.log('')
+reviewQuestion8.textContent = '8. Final Expression'
+console.log('8. Final Expression')
+console.log('the third parameter of a "for" loop, generally the iterator parameter')
 
-reviewAnswer8.textContent = `- `
+reviewAnswer8.textContent = `- the third parameter of a "for" loop, generally the iterator parameter`
 console.log('')
 
 
 
 
 //9.
-reviewQuestion9.textContent = '9. '
-console.log('9. ')
-console.log('')
+reviewQuestion9.textContent = '9. classList'
+console.log('9. classList')
+console.log('JavaScript property used to add, remove, or toggle classes on and off of HTML elements')
 
-reviewAnswer9.textContent = `- `
+reviewAnswer9.textContent = `- JavaScript property used to add, remove, or toggle classes on and off of HTML elements`
 console.log('')
 
 
 
 
 //10.
-reviewQuestion10.textContent = '10. '
-console.log('10. ')
-console.log('')
+reviewQuestion10.textContent = '10. Spread Syntax in a function call'
+console.log('10. Spread Syntax in a function call')
+console.log(`
+let spreadSyntax = (a, b, c) => {
+    return a * b + c
+}
+let nums = [1, 2, 3];
+let sum = spreadSyntax(...nums)
+console.log(sum)
+`)
 
-reviewAnswer10.textContent = `- `
+let spreadSyntax = (a, b, c) => {
+    return a * b + c
+}
+let nums = [1, 2, 3];
+let sum = spreadSyntax(...nums)
+console.log(sum)
+spreadSyntax()
+
+reviewAnswer10.textContent = `
+let spreadSyntax = (a, b, c) => {
+   return a * b + c 
+}
+let nums = [1, 2, 3];
+let sum = spreadSyntax(...nums)
+console.log(sum)
+`
 console.log('')
 
 
