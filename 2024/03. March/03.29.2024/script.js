07a88a60d6e26520fa44eb851dc5255899404e46
Leanne Keenan <l_keenan@outlook.com>
2024-03-29 10:39:59 -0700

    Added Daily Training Session

---
+++ 2024/03. March/03.29.2024/script.js
@@ -0,0 +1,119 @@
+/*ANKI Review: */
+console.log('ANKI Review: Codewars Challenges')
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+
+//1.
+codewarsQuestion1.textContent = '1. Add Length - write a function that accepts a string as an arguement and returns it as an array with the length of each word appended to each array element'
+console.log(`1. Add Length - write a function that accepts a string as an arguement and returns it as an array with the length of each word appended to each array element`)
+console.log(``)
+
+codewarsAnswer1.textContent = `- `
+
+let addLength = (string) => {
+    let words = string.split(' ');
+    let word = words.split('')
+    let length = word.length;
+    console.log(word + " " + length)
+
+}
+
+addLength('hello world')
+
+console.log('')
+
+//2.
+codewarsQuestion2.textContent = '2. '
+console.log(`2. `)
+console.log(``)
+
+codewarsAnswer2.textContent = `- `
+console.log('')
+
+//3.
+codewarsQuestion3.textContent = '3. '
+console.log(`3. `)
+console.log(``)
+
+codewarsAnswer3.textContent = `- `
+console.log('')
+
+//4.
+codewarsQuestion4.textContent = '4. '
+console.log(`4. `)
+console.log(``)
+
+codewarsAnswer4.textContent = `- `
+console.log('')
+
+//5.
+codewarsQuestion5.textContent = '5. '
+console.log(`5. `)
+console.log(``)
+
+codewarsAnswer5.textContent = `- `
+console.log('')
+
+codewarsQuestion6.textContent = '6.'
+console.log(`6. `)
+console.log(``)
+
+codewarsAnswer6.textContent = `- `
+console.log('')
+
+codewarsQuestion7.textContent = '7.'
+console.log(`7. `)
+console.log(``)
+
+codewarsAnswer7.textContent = `- `
+console.log('')
+
+codewarsQuestion8.textContent = '8.'
+console.log(`8. `)
+console.log(``)
+
+codewarsAnswer8.textContent = `- `
+console.log('')
+
+codewarsQuestion9.textContent = '9.'
+console.log(`9. `)
+console.log(``)
+
+codewarsAnswer9.textContent = `- `
+console.log('')
+
+codewarsQuestion10.textContent = '10.'
+console.log(`10.`)
+console.log(``)
+codewarsAnswer10.textContent = `- `
+
+
+
+
+
+
+
+
+
+
