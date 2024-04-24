881a9587f0bb9276016be6063b6a2c6b4e5d4b67
Leanne Keenan <l_keenan@outlook.com>
2024-04-24 12:01:24 -0700

    Added Daily Training SEssion

---
+++ 2024/04. April/04.24.2024/script.js
@@ -0,0 +1,240 @@
+/*ANKI Review: */
+console.log('ANKI Review: Command Line Interface Terminal Commands')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1. `/.bash_profile'
+console.log('1. `/.bash_profile')
+console.log('CLI that is used to locate files used to store environment settings. When a session starts, the contents load before commands are executed')
+
+reviewAnswer1.textContent = `- CLI that is used to locate files used to store environment settings. When a session starts, the contents load before commands are executed`
+console.log('')
+
+
+
+
+//2.
+reviewQuestion2.textContent = '2. env | grep VARIABLE'
+console.log('2. env | grep VARIABLE')
+console.log('stands for "Environment global regular expression print" and is a CLI used to display the valur of a dingle environment variable')
+
+reviewAnswer2.textContent = `- stands for "Environment global regular expression print" and is a CLI used to display the valur of a dingle environment variable`
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. stderr'
+console.log('3. stderr')
+console.log('stands for "Standard Error" and is a CLI used to catch errors in the environment')
+
+reviewAnswer3.textContent = `- stands for "Standard Error" and is a CLI used to catch errors in the environment`
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. env'
+console.log('4. env')
+console.log('stands for "environment" and is a CLI used to return a list of environment elements')
+
+reviewAnswer4.textContent = `- stands for "environment" and is a CLI used to sreturn a list of environment elements`
+console.log('')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. cd ~'
+console.log('5. cd ~')
+console.log('stands for "change directory to home" and is a CLI used to move from the current dorectory a user is in to the containing folder. If the file is not ina  containing folder, you are directed to the desktop')
+
+
+reviewAnswer5.textContent = `- stands for "change directory to home" and is a CLI used to move from the current dorectory a user is in to the containing folder. If the file is not ina  containing folder, you are directed to the desktop`
+console.log('')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. stdin'
+console.log('6. stdin')
+console.log('stands for "standard input" and is a CLI for information inputted through the terminal througha  keyboard or input device')
+
+reviewAnswer6.textContent = `- stands for "standard input" and is a CLI for information inputted through the terminal througha  keyboard or input device`
+console.log('')
+
+
+
+
+//7.
+reviewQuestion7.textContent = '7. grep -i'
+console.log('7. grep -i')
+console.log('global regular expression print insensitive meaning the casing will not affect the command')
+
+reviewAnswer7.textContent = `- global regular expression print insensitive meaning the casing will not affect the command`
+console.log('')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. sed'
+console.log('8. sed')
+console.log('stands for "stream editor" and is a CLI that accepts standard input and modifies it based on and expression before displaying it as output data')
+
+reviewAnswer8.textContent = `- stands for "stream editor" and is a CLI that accepts standard input and modifies it based on and expression before displaying it as output data`
+console.log('')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. mv'
+console.log('9. mv')
+console.log('stands for "move" and is a CLI used to move files into directories')
+
+reviewAnswer9.textContent = `- stands for "move" and is a CLI used to move files into directories`
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. uniq'
+console.log('10. uniq')
+console.log('stands for "unique" and is used to give variables uniques identifiers, removing duplicates')
+
+reviewAnswer10.textContent = `- stands for "unique" and is used to give variables uniques identifiers, removing duplicates`
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1. '
+console.log('1. ')
+console.log('')
+
+codewarsAnswer1.textContent = `- `
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2. '
+console.log('2. ')
+console.log('')
+
+codewarsAnswer2.textContent = `- `
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3. '
+console.log('3. ')
+console.log('')
+
+codewarsAnswer3.textContent = `- `
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4. '
+console.log('4. ')
+console.log('')
+
+codewarsAnswer4.textContent = `- `
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5. '
+console.log('5. ')
+console.log('')
+
+codewarsAnswer5.textContent = `- `
+console.log('')
+
+
+
+// codewarsQuestion6.textContent = '6.'
+// codewarsAnswer6.textContent = `- `
+// codewarsQuestion7.textContent = '7.'
+// codewarsAnswer7.textContent = `- `
+// codewarsQuestion8.textContent = '8.'
+// codewarsAnswer8.textContent = `- `
+// codewarsQuestion9.textContent = '9.'
+// codewarsAnswer9.textContent = `- `
+// codewarsQuestion10.textContent = '10.'
+// codewarsAnswer10.textContent = `- `
+
+
+
+
+
+
+
+
+
+
