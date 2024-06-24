b517e192ce4e2695cbdf9c74e61eea897d601da2
Leanne Keenan <l_keenan@outlook.com>
2024-06-24 15:11:46 -0700

    Completed Daily Codewars challenges

---
+++ 2024/06. June/06.24.2024/script.js
@@ -0,0 +1,283 @@
+/*ANKI Review: */
+console.log('ANKI Review:')
+let reviewQuestion1 = document.getElementById('rq1')
+let reviewQuestion2 = document.getElementById('rq2')
+let reviewQuestion3 = document.getElementById('rq3')
+let reviewQuestion4 = document.getElementById('rq4')
+let reviewQuestion5 = document.getElementById('rq5')
+let reviewQuestion6 = document.getElementById('rq6')
+let reviewQuestion7 = document.getElementById('rq7')
+let reviewQuestion8 = document.getElementById('rq8')
+let reviewQuestion9 = document.getElementById('rq9')
+let reviewQuestion10 = document.getElementById('rq10')
+
+let reviewAnswer1 = document.getElementById('ra1')
+let reviewAnswer2 = document.getElementById('ra2')
+let reviewAnswer3 = document.getElementById('ra3')
+let reviewAnswer4 = document.getElementById('ra4')
+let reviewAnswer5 = document.getElementById('ra5')
+let reviewAnswer6 = document.getElementById('ra6')
+let reviewAnswer7 = document.getElementById('ra7')
+let reviewAnswer8 = document.getElementById('ra8')
+let reviewAnswer9 = document.getElementById('ra9')
+let reviewAnswer10 = document.getElementById('ra10')
+
+
+
+
+
+//1.
+reviewQuestion1.textContent = '1. What is a legend'
+console.log('1. What is a legend')
+console.log('The legend is an HTML tag used within HTML fieldset tags to provide capttions or titles to the content to help organize, label, and enhance accessability to web forms')
+
+reviewAnswer1.textContent = `- The legend is an HTML tag used within HTML fieldset tags to provide capttions or titles to the content to help organize, label, and enhance accessability to web forms`
+console.log('')
+
+
+
+
+//2.
+reviewQuestion2.textContent = '2. What is the difference between id anc class naming?'
+console.log('2. What is the difference between id anc class naming?')
+console.log('In HTML, a class is a identification tool used to allow elements to belong to a shared froup.  The class tag in HTML allows elements an additional 10 points of specificity each and can ue used in the CSS hiearchy to override rules, and can be reused on multiple elements. Similarly, IDs are used to provide unique identification to page elements so they can be identified independently.  The IS adds 100 points of specificity and is not recommended to be used to add CSS to elements, but instead be reserved for JavaScript access to the element')
+
+reviewAnswer2.textContent = `- In HTML, a class is a identification tool used to allow elements to belong to a shared froup.  The class tag in HTML allows elements an additional 10 points of specificity each and can ue used in the CSS hiearchy to override rules, and can be reused on multiple elements. Similarly, IDs are used to provide unique identification to page elements so they can be identified independently.  The IS adds 100 points of specificity and is not recommended to be used to add CSS to elements, but instead be reserved for JavaScript access to the element`
+console.log('')
+
+
+
+
+
+//3.
+reviewQuestion3.textContent = '3. data-* attribute'
+console.log('3. data-* attribute')
+console.log('The "data-*" attribute allows programmers the opportunity to create their own unique tag names for page content')
+
+reviewAnswer3.textContent = `- The "data-*" attribute allows programmers the opportunity to create their own unique tag names for page content`
+console.log('')
+
+
+
+
+//4.
+reviewQuestion4.textContent = '4. <caption>'
+console.log('4. <caption>')
+console.log('The <caption> tag is a semantic HTML element used to add a caption description to table content')
+
+reviewAnswer4.textContent = `- The <caption> tag is a semantic HTML element used to add a caption description to table content`
+console.log('')
+
+
+
+
+//5.
+reviewQuestion5.textContent = '5. target=_top'
+console.log('5. target=_top')
+console.log('The "target=_top" funtcion is a built in feature that llows for the content of a website link to open in outside of the viewport of the OS window.')
+
+
+reviewAnswer5.textContent = `- The "target=_top" funtcion is a built in feature that llows for the content of a website link to open in outside of the viewport of the OS window.`
+console.log('')
+
+
+
+
+//6.
+reviewQuestion6.textContent = '6. What is the keyword "inherit" for?'
+console.log('6. What is the keyword "inherit" for?')
+console.log('A value in CSS used to allow rule properties of a child element to inherit the proerty values from their parent without having to rewrite the value heeling code DRY.  The concept is ued in HTML in terms of nesting, but is not a rule in and of itself.')
+
+reviewAnswer6.textContent = `- A value in CSS used to allow rule properties of a child element to inherit the proerty values from their parent without having to rewrite the value heeling code DRY.  The concept is ued in HTML in terms of nesting, but is not a rule in and of itself.`
+console.log('')
+
+
+
+
+//7.
+reviewQuestion7.textContent = '7. target="_blank"'
+console.log('7. target="_blank"')
+console.log('')
+
+reviewAnswer7.textContent = `- target="_blank" is a HTML function that opens the content of a website in a new OS window outside of the orignal website`
+console.log('target="_blank" is a HTML function that opens the content of a website in a new OS window outside of the orignal website')
+
+
+
+
+//8.
+reviewQuestion8.textContent = '8. What is the viewport meta tag syntax?'
+console.log('8. What is the viewport meta tag syntax?')
+console.log('<meta name="viewport" content="width=device-width" initial-scale="1.0" minimum-scale="1.0>')
+
+reviewAnswer8.textContent = `- <meta name="viewport" content="width=device-width" initial-scale="1.0" minimum-scale="1.0>`
+console.log('')
+
+
+
+
+//9.
+reviewQuestion9.textContent = '9. What are the three viewport property options'
+console.log('9. What are the three viewport property options')
+console.log('')
+
+reviewAnswer9.textContent = `- initial sets the scale of the content, minimum sets a minimum szie but allows for users to zoom in as needed, user-scaleable allows the user to set the viewport size `
+console.log('')
+
+
+
+
+//10.
+reviewQuestion10.textContent = '10. What is W3C'
+console.log('10. What is W3C')
+console.log('W3C stands for World Wide Web Consortium.  Is it the group of people founded by Sir Tim Berners Lee, and they are the people responsible for the rules and regulations regarding coding languages, syntax, rules and applications')
+
+reviewAnswer10.textContent = `- W3C stands for World Wide Web Consortium.  Is it the group of people founded by Sir Tim Berners Lee, and they are the people responsible for the rules and regulations regarding coding languages, syntax, rules and applications`
+console.log('')
+
+
+let codewarsQuestion1 = document.getElementById('cwq1')
+let codewarsQuestion2 = document.getElementById('cwq2')
+let codewarsQuestion3 = document.getElementById('cwq3')
+let codewarsQuestion4 = document.getElementById('cwq4')
+let codewarsQuestion5 = document.getElementById('cwq5')
+let codewarsQuestion6 = document.getElementById('cwq6')
+let codewarsQuestion7 = document.getElementById('cwq7')
+let codewarsQuestion8 = document.getElementById('cwq8')
+let codewarsQuestion9 = document.getElementById('cwq9')
+let codewarsQuestion10 = document.getElementById('cwq10')
+
+let codewarsAnswer1 = document.getElementById('cwa1')
+let codewarsAnswer2 = document.getElementById('cwa2')
+let codewarsAnswer3 = document.getElementById('cwa3')
+let codewarsAnswer4 = document.getElementById('cwa4')
+let codewarsAnswer5 = document.getElementById('cwa5')
+let codewarsAnswer6 = document.getElementById('cwa6')
+let codewarsAnswer7 = document.getElementById('cwa7')
+let codewarsAnswer8 = document.getElementById('cwa8')
+let codewarsAnswer9 = document.getElementById('cwa9')
+let codewarsAnswer10 = document.getElementById('cwa10')
+
+/*Codewars Challenges*/
+console.log('Codewars Challenges')
+
+
+//1.
+codewarsQuestion1.textContent = '1. list filtering - create a function that takes a list of non-negative numbers and leters and returns a new list with the letters filtered out'
+console.log('1. list filtering - create a function that takes a list of non-negative numbers and leters and returns a new list with the letters filtered out')
+console.log('')
+
+let listFilter = (list) => {
+    console.log(list.filter(item => typeof item === 'number'))
+    }
+    listFilter([ ])
+    listFilter(['h', 3, 1, 1, 0])
+codewarsAnswer1.textContent = `- `
+console.log('')
+
+
+
+
+//2.
+codewarsQuestion2.textContent = '2. Pipe problem - given a list of unique numbers, return a new list so the values increments by 1 for each index from the minimum value to the max'
+console.log('2. Pipe problem - given a list of unique numbers, return a new list so the values increments by 1 for each index from the minimum value to the max')
+console.log('')
+
+let pipeProblem = (numbers) => {
+    let min = Math.min(...numbers);//get the lowest value
+    let max = Math.max(...numbers);//get the highest value
+
+    let increment = []; //empty value to hold return
+
+    for(let i = min; i <= max; i++) {  //when min is less or equal to max
+        increment.push(i) //push the value of min to increment until you reach the max value
+    }
+    console.log(increment) //print the new array
+}
+
+pipeProblem([6, 8, 2, 5, 3, 0])
+
+codewarsAnswer2.textContent = `- `
+console.log('')
+
+
+
+
+//3.
+codewarsQuestion3.textContent = '3. remove duplicates from a list - frdine a function that removes duplicates from an array of non-negative numbers and returns a new list with the values unrepeated'
+console.log('3. remove duplicates from a list - frdine a function that removes duplicates from an array of non-negative numbers and returns a new list with the values unrepeated')
+console.log('')
+
+let removeDuplicates = (list) => {
+    let result = new Set([...list])
+
+    console.log([...result])
+}
+removeDuplicates([1, 2, 1, 3, 1, 4, 5, 5, 3, 5, 7, 7, 3, 0])
+
+codewarsAnswer3.textContent = `- `
+console.log('')
+
+
+
+
+
+//4.
+codewarsQuestion4.textContent = '4. Cypher - create a function that takes a string and returns the string as a cyhper that shifts to the right in the alphabet by 13 letters, meaning a becomes m an dso forth'
+console.log('4. Cypher - create a function that takes a string and returns the string as a cyhper that shifts to the right in the alphabet by 13 letters, meaning a becomes m an dso forth')
+console.log('')
+
+let cypher = (string) => {
+    console.log(string.replace(/[[a-z]/ig, character => String.fromCharCode((character.charCodeAt(0) + 13))));
+    
+}
+cypher('hello')
+codewarsAnswer4.textContent = `- `
+console.log('')
+
+
+
+
+
+//5.
+codewarsQuestion5.textContent = '5. ATM - qrite a function that simulates an ATM that honors withdrawls of 1500 or less with the least amount of bank notes to create the sum total'
+console.log('5. ATM - qrite a function that simulates an ATM that honors withdrawls of 1500 or less with the least amount of bank notes to create the sum total')
+console.log('')
+
+let ATM = (withdrawl) => {
+    let banknotes = [100, 50, 20, 10, 5, 1];
+    let total = 0
+
+    for(let i = 0; i < banknotes.length; i++) {
+      total += Math.floor(withdrawl / banknotes[i]);
+      withdrawl %= banknotes[i]
+    }
+    console.log(total)
+}
+ATM(500)
+
+codewarsAnswer5.textContent = `- `
+console.log('')
+
+
+
+// codewarsQuestion6.textContent = '6.'
+// codewarsAnswer6.textContent = `- `
+// codewarsQuestion7.textContent = '7.'
+// codewarsAnswer7.textContent = `- `
+// codewarsQuestion8.textContent = '8.'
+// codewarsAnswer8.textContent = `- `
+// codewarsQuestion9.textContent = '9.'
+// codewarsAnswer9.textContent = `- `
+// codewarsQuestion10.textContent = '10.'
+// codewarsAnswer10.textContent = `- `
+
+
+
+
+
+
+
+
+
+
